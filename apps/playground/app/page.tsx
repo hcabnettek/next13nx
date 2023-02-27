84dbd41e17f6497f596cf70a2d3b6b3725e83bd1
Chris Kettenbach <ckettenb@scgcontractor.com>
2023-02-27 08:11:03 -0800

    chore: reproducing a build issue

---
+++ apps/playground/app/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function Page() {
+  return (
+    <div className="space-y-8">
+      <h1 className="text-xl font-medium text-gray-300">Root Page</h1>
+
+      <div className="space-y-10 text-white">
+        <h2>Sweet</h2>
+      </div>
+    </div>
+  );
+}
